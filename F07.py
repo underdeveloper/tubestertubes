6bed1f14c87b608434b52bf47e679a592f418f5e
underdeveloper <der.sulthana@gmail.com>
2020-04-11 16:21:44 +0700

    F07 yang baru nih

---
+++ F07.py
@@ -0,0 +1,20 @@
+# F07 - PEMBELIAN TIKET
+# 
+import auxilliary as aux
+
+def beli_tiket(username, user, wahana, pembelian_tiket, kepemilikan_tiket):
+    # PROCEDURE beli_tiket (input user : string, input user : FileCSV, input wahana : FileCSV
+    #                      input/output : pembelian_tiket, input/output : kepemilikan_tiket)
+    # I.S. pembelian_tiket abstrak, username sudah pasti ada di database user
+    # F.S. Jika input lolos ujian, pembelian_tiket akan ditambah sesuai pembelian tiket yang dilakukan pengguna.
+    # KAMUS LOKAL
+
+    # ALGORITMA
+    return True # temporer saja :)
+    
+    # 1. Meminta input berikut: ID wahana, tanggal hari ini, jumlah tiket yang dibeli
+    # 2. Validasi input!
+    # 3. Mengecek apabila pengguna memenuhi persyaratan wahana
+    # 4. Mengecek apabila saldo pengguna sudah cukup
+    # 5. Jika lolos semua pengecekan, ditulis data pembelian ke pembelian_tiket
+    #    dan tambahkan tiket ke kepemilikan_tiket
