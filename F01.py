3f929f27bb93fa6eee096108592c4898e6f17de4
underdeveloper <der.sulthana@gmail.com>
2020-04-13 15:02:16 +0700

    File F01 sudah diperbaharui, sesuai syarat hopefully sekarang.

--- F01.py
+++ F01.py
@@ -1,100 +1,109 @@
 # Program F01
 # Me-load ke-7 file .csv ke sistem
 
-# KAMUS
-class file:
-    count = 7
-    description = ("File User", "File Daftar Wahana", "File Pembelian Tiket", "File Penggunaan Tiket", "File Kepemilikan Tiket", "File Refund Tiket", "File Kritik dan Saran")
-    name = ["*" for i in range (count)]
-    data = ["*" for i in range (count)]
-# Di atas ini adalah "file", suatu tipe bentukan
-# Saat modul ini dijalankan, data ke-7 file .csv akan disimpan ke dalam file.data
-# file.data inilah yang kemudian akan diakses, diubah valuenya, dst selama program dijalankan
-# Modul lain akan akan mengurus penyimpanan kembali file.data ke-7 file .csv
-
-# ALGORITMA PROGRAM UTAMA
-
 import os
-import csv
+import csv  # Library eksternal untuk mengakses file
+import auxilliary as aux  # Library internal, berisi fungsi pembantu
+
+# KAMUS
+class Rekaman:
+    # Tipe bentukan kamus data yang akan dipakai.
+    # Rekaman : < name : string
+    #             columns : int
+    #             rows : int
+    #             data : array [0..rows-1] of array [0..columns-1] of string
+    def __init__(self, name="", columns=1, rows=1):
+        self.name = name
+        self.columns = columns
+        self.rows = rows
+        self.data = [["*" for i in range(columns)] for j in range(rows)]
 
 # REALISASI FUNGSI/PROSEDUR
 
-def main():
-    # procedure main (output data : file)
+# File-file yang kita perlukan. bisa diganti nanti.
+filedescs = ["User", "Daftar Wahana", "Pembelian Tiket", "Penggunaan Tiket", "Kepemilikan Tiket", "Refund Tiket", "File Kritik dan Saran"]
+files = []
+
+def main(filedescs):
+    # procedure main (input filenames : array of string
+    # output data : file)
     # I.S. file.data terdefinisi sembarang
     # F.S. ke-7 file .csv di-load ke file.data
     # KAMUS LOKAL
     # i : integer
     # reader : _csv.reader object
     # ALGORITMA
-    for i in range (file.count):
-        # Masukkan <nama file> yang akan di-load
-        file.name[i] = str(input("Masukkan nama " + file.description[i] + ": "))
-        # Load <nama file>.csv ke file.data
-        with open(os.path.dirname(__file__) + "\\" + str(file.name[i]), mode = 'r') as f:
-            reader = csv.reader(f)
-            file.data[i] = list(reader)
-    print("")
-    print("File perusahaan Willy Wangky's Chocolate Factory telah di-load.")
+    
+    file_amount = aux.arr_length(filedescs)
+    files = [Rekaman() for i in range(file_amount)] # Array berisi kamus data
+    
+    for i in range(file_amount):
+        # Pemasukan nama file eksternal (csv) yang akan dimuat
+        files[i].name = input("Masukkan nama file " + filedescs[i] + ": ")
 
-def use(filename):
-    # function use (filename : string) -> string
-    # Memberikan copy file yang diminta dari versi yang ada di file.name
-    # KAMUS LOKAL
-    # i : integer
-    # ALGORITMA
-    try:
-        for i in range (len(file.name)):
-            if (str(filename) == file.name[i]):
-                return file.data[i]
-            else:
-                raise ValueError
-    except ValueError:
-        print("ERROR: Invalid filename.")
+        # Memuatkan <ext_file>.csv ke kamus data
+        with open(os.path.dirname(__file__) + "\\" + files[i].name, 'r') as f:
+            file_contents = list(csv.reader(f))
 
-def store(filename):
-    # function store (filename : string) -> string
-    # Meng-update salah satu elemen file.name yang sesuai dengan nama file yang di-input
-    # KAMUS LOKAL
-    # i : integer
-    # ALGORITMA
-    try:
-        for i in range (len(file.name)):
-            if(str(filename) == file.name[i]):
-                file.name[i] = filename
-            else:
-                raise ValueError
-    except ValueError:
-        print("ERROR: Invalid filename.")
+            files[i].columns = aux.arr_length(file_contents[0])
+            files[i].rows = aux.arr_length(file_contents)
 
-def findidx(array, colname):
-    # function findidx (colname : string) -> integer
-    # Mencari nomor indeks suatu kolom bernama colname pada array array
-    # KAMUS LOKAL
-    # i : integer
-    # ALGORITMA
-    for i in range (len(array[0])):
-        if (str(array[0][i]) == str(colname)):
-            return int(i)
+            files[i].data = file_contents
+    
+    print("\nFile-file telah dimuat.\n")
 
-def finddata(array, colname, keyword):
-    #function finddata (array : array of array of string, colname : string, keyword : string) -> array of string
-    # Mencari data yang tersimpan dalam array
-    # KAMUS LOKAL
-    # i, colidx : integer
-    # datafound : array of string
-    # isFound : boolean
-    # ALGORITMA
-    for i in range (len(array[0])):
-        if (array[0][i] == colname):
-            colidx = int(i)
-    isFound = False
-    for i in range (len(array)):
-        if (array[i][colidx] == keyword):
-            datafound = array[i]
-            isFound = True
-    if (isFound == True):
-        return datafound
-    else:
-        datafound = []
-        return datafound
+def get_data(filename):
+    # function get_data (filename : string) -> array of array of string
+    # Mengeluarkan tabel yang merupakan isi dari data yang telah dimuat.
+    # Syarat: File bernama filename sudah pasti telah dimuat.
+
+    found = False
+
+    for file in files:
+        if file.name == filename:
+            return file.data
+    
+    if not found:
+        return None
+
+def store_data(filename, new_table):
+    # prosedur store_data(input filename : string, output file : Rekaman)
+    # I.S. file belum diganti
+    # F.S. file telah diganti user
+    # Syarat: File bernama filename sudah pasti telah dimuat.
+
+    for file in files:
+        if file.name == filename:
+            file.data = new_table
+
+# def findidx(array, colname):
+#     # function findidx (colname : string) -> integer
+#     # Mencari nomor indeks suatu kolom bernama colname pada array array
+#     # KAMUS LOKAL
+#     # i : integer
+#     # ALGORITMA
+#     for i in range (len(array[0])):
+#         if (str(array[0][i]) == str(colname)):
+#             return int(i)
+
+# def finddata(array, colname, keyword):
+#     #function finddata (array : array of array of string, colname : string, keyword : string) -> array of string
+#     # Mencari data yang tersimpan dalam array
+#     # KAMUS LOKAL
+#     # i, colidx : integer
+#     # datafound : array of string
+#     # isFound : boolean
+#     # ALGORITMA
+#     for i in range (len(array[0])):
+#         if (array[0][i] == colname):
+#             colidx = int(i)
+#     isFound = False
+#     for i in range (len(array)):
+#         if (array[i][colidx] == keyword):
+#             datafound = array[i]
+#             isFound = True
+#     if (isFound == True):
+#         return datafound
+#     else:
+#         datafound = []
+#         return datafound
