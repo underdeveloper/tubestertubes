fcb002d8aa535d8e73b79ce3ec50e24f7305810b
baskorooo <63135932+baskorooo@users.noreply.github.com>
2020-04-13 17:19:58 +0700

    Update F05
    Update dari F05, semoga udah bener

--- F05.py
+++ F05.py
@@ -6,6 +6,10 @@
 #ALGORITMA PROGRAM UTAMA
 
 import F01 as load
+<<<<<<< Updated upstream
+=======
+import auxilliary as aux
+>>>>>>> Stashed changes
 
 #REALISASI FUNGSI/PROSEDUR
 def main(userfile):
@@ -21,6 +25,7 @@ def main(userfile):
     isUserValid = False
     while isUserValid == False :
         y = str(input("Masukkan username :"))
+<<<<<<< Updated upstream
         finduser = load.finddata(userfile,"Username",y)
         if finduser != [] :
             isUserValid = True
@@ -28,12 +33,26 @@ def main(userfile):
             print("Nama Pemain: " + finduser[load.findidx(userfile,"Nama")])
             print("Tinggi Pemain: " + finduser[load.findidx(userfile,"Tinggi_Badan")])
             print("Tanggal Lahir Pemain: " + finduser[load.findidx(userfile,"Tanggal_Lahir")])
+=======
+        finduser = load.find_baris_first(userfile,"Username",y)
+        if finduser != [] :
+            isUserValid = True
+            print("")
+            print("Nama Pemain: " + finduser[load.find_idx(userfile,"Nama")])
+            print("Tinggi Pemain: " + finduser[load.find_idx(userfile,"Tinggi_Badan")])
+            print("Tanggal Lahir Pemain: " + finduser[load.find_idx(userfile,"Tanggal_Lahir")])
+>>>>>>> Stashed changes
         else :
             isUserValid = False
             print("Username tidak ditemukan, silahkan coba lagi")
     user = []
+<<<<<<< Updated upstream
     user.append(userfile[0])
     user.append(finduser)
+=======
+    aux.konsDot(user,[userfile[0]])
+    aux.konsDot(user,[finduser])
+>>>>>>> Stashed changes
     return user
 
 
