2f58aea663aa97390ee69c7b2cb9e767c0406770
underdeveloper <der.sulthana@gmail.com>
2020-04-20 09:22:54 +0700

    F01 dan F02 auto

--- F01.py
+++ F01.py
@@ -48,55 +48,53 @@ def main():
     print("File perusahaan Willy Wangky's Chocolate Factory telah di-load.")
     print("")
 
+def main_auto():
+    # procedure main_auto (output files : array of Rekaman)
+    # I.S. file.data terdefinisi sembarang
+    # F.S. ke-7 file .csv di-load ke files
+    # main_auto() merupakan prosedur sama dengan main(), tetapi diotomasikan.
+    # Prosedur ini dipakai untuk masa testing program ini, dan akan dimatikan
+    # saat program final dikeluarkan.
+
+    actual_filenames = ["user.csv", "wahana.csv", "pembelian.csv", 
+    "penggunaan.csv", "tiket.csv", "refund.csv", "kritiksaran.csv"]
+    for i in range(flib.length(actual_filenames)):
+        files[i].name = actual_filenames[i]
+        with open(os.path.dirname(__file__) + "\\" + actual_filenames[i], mode='r') as f:
+            reader = list(csv.reader(f))
+            files[i].rows = flib.length(reader)
+            files[i].columns = flib.length(reader[0])
+            files[i].data = reader
+    print("\nFile perusahaan Willy Wangky's Chocolate Factory telah di-load.\n")
+
 def use(filename):
-    # function use (filename : string) -> string
-    # Memberikan copy file yang diminta dari versi yang ada di file.name
+    # function use (filename : string) -> array of array of string
+    # Memberikan copy file yang diminta dari versi yang ada di files
+    # Syarat: Filename sudah benar.
     # KAMUS LOKAL
-    # i, j, k : integer
-    # isFound : boolean
-    # filefound : array [0..files[i].rows] of array [0..files[i].columns] of string
+    # i : integer
     # ALGORITMA
-    try:
-        i = 0
-        isFound = False
-        while ((i < filecount) and (isFound == False)):
-            if (str(files[i].name) == filename):
-                isFound = True
-                filefound = [["*" for k in range (files[i].columns)] for j in range (files[i].rows)]
-                filefound = files[i]
-            i = i + 1
-        if (isFound == False):
-            raise ValueError
-        return filefound
-    except ValueError:
-        print("ERROR: Invalid filename.")
+    for i in range(0, filecount+1):
+        if (files[i].name == filename):
+            return files[i]
     # APLIKASI
     # (pada modul lain)
     # import F01 as load
     # load.use("user.csv")
     # >> files[0]
 
-def store(filename, new_table):
-    # procedure store (input filename : string, input new_table : array of array of string)
-    # Meng-update salah satu elemen file.name yang sesuai dengan nama file yang di-input
+def store(filename, table_baru):
+    # procedure store (input filename : string, output files[i] : Rekaman)
+    # Menyetor table_baru ke file bernama filename.
+    # Syarat: Filename sudah benar.
     # KAMUS LOKAL
     # i : integer
-    # isStored : boolean
     # ALGORITMA
-    i = 0
-    isStored = False
-    try:
-        while ((i < filecount) and (isStored == False)):
-            if (files[i].name == filename):
-                files[i].rows = flib.length(new_table)
-                files[i].columns = flib.length(new_table[0])
-                files[i].data = new_table
-                isStored = True
-            i = i + 1
-        if (isStored == False):
-            raise ValueError
-    except ValueError:
-        print("ERROR: Invalid filename.")
+    for i in range(0, filecount):
+        if files[i].name == filename:
+            files[i].rows = flib.length(table_baru)
+            files[i].columns = flib.length(table_baru[0])
+            files[i].data = table_baru
     # APLIKASI
     # (pada modul lain)
     # import F01 as load
