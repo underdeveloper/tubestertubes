bfa28bf2272cda41bbc532d255cce7796a60de65
flckrs <55950188+flckrs@users.noreply.github.com>
2020-04-22 21:45:13 +0700

    yang fungsi use di F01 kok gabisa dijalanin yak, eror list index out of range
    def main udh bener, cumen return nya gatau ke mana hehehe

--- F14.py
+++ F14.py
@@ -15,22 +15,31 @@ import F01 as load
 def main(userfile):
     # Program utama F14
     # Admin memasukkan ID Wahana
-    wahana = load.use ("wahana.csv")
-    user = load.use ("penggunaan.csv")
+    user = load.use (userfile)
 
     id_wahana = str(input("Masukkan ID Wahana: "))
-    id_wahana_found = aux.find_baris_first(wahana.data, "ID_Wahana", id_wahana)
+    id_wahana_found = aux.find_baris_all(user.data, "ID_Wahana", id_wahana)
 
     while id_wahana_found == [] :
         id_wahana = input("Tidak ditemukan wahana dengan ID \"" + id_wahana + '\". Mohon diulang: ')
-        id_wahana_found = aux.find_baris_first(wahana.data, "ID_Wahana", id_wahana)
+        id_wahana_found = aux.find_baris_all(user.data, "ID_Wahana", id_wahana)
 
     print("Riwayat: ")
     print (" ")
-    if id_wahana_found != [] :
-        search = aux.find_baris_all(user.data, "ID_Wahana", id_wahana)
-        tanggal = aux.find_kolom(search, "Tanggal_Penggunaan")
-        username = aux.find_kolom(search, "Username")
-        jmltiket = aux.find_kolom(search, "Jumlah_Tiket")
-        print (tanggal, "| ", username, "| ", jmltiket)
+    # if id_wahana_found != [] : <<tidak perlu, sudah divalidasi>>
+    j = 0
+    i = 0
+    arr1 = [0 for i in range(len(id_wahana_found))]
+    username = [0 for j in range(len(id_wahana_found))]
+    tanggal_pakai = [0 for j in range(len(id_wahana_found))]
+    jumlah_tiket = [0 for j in range(len(id_wahana_found))]
 
+    for i in range(len(id_wahana_found)):
+        arr1[i] = id_wahana_found[i]
+    for j in range(len(arr1[i])):
+        username = arr1[j][0]
+        tanggal_pakai = arr1[j][1]
+        jumlah_tiket =  arr1[j][3]
+        print(tanggal_pakai, "| ", username, "| ", jumlah_tiket)
+
+    return
