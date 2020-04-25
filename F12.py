a03d51fe649c1fc61d5e0b863e687f5f9f6fa12b
flckrs <steflx0510@gmail.com>
2020-04-25 13:58:07 +0700

    udh

--- F12.py
+++ F12.py
@@ -6,35 +6,37 @@
 
 
 # KAMUS
-class user :
-    datacount = 5
-    datadesc = ("ID_Wahana", "Nama_Wahana", "Harga_Tiket",
-                "Batasan_Umur", "Batasan_Tinggi")
-    data = ["*" for i in range (datacount)]
+
 
 # ALGORITMA
-
+import auxilliary as aux
+import F01 as load
 
 # REALISASI FUNGSI/PROSEDUR
-def main(userfile):
+def main():
     # Fungsi utama F12
     # Semua masukkan diasumsikan valid
-
+    wahana = load.use("wahana.csv")
+    array_wahana_baru = ["ID_Wahana" , "Nama_Wahana" , "Harga_Tiket" 
+    , "Batasan_Umur" , "Batasan_Tinggi"]
+    
     print ("Masukkan Informasi Wahana yang ditambahkan: ", end = " ")
-    for i in range (user.datacount):
-        if (user.datadesc[i] == "ID_Wahana") :
-            user.data[i] = str(input("Masukkan " + user.datadesc[i] + ": "))
-        elif (user.datadesc[i] == "Nama_Wahana") :
-            user.data[i] = str(input("Masukkan " + user.datadesc[i] + ": "))
-        elif (user.datadesc[i] == "Harga_Tiket"):
-            user.data[i] = str(input("Masukkan " + user.datadesc[i] + ": "))
-        elif (user.datadesc[i] == "Batasan_Umur"):
-            user.data[i] = str(input("Masukkan " + user.datadesc[i] + ": "))
-        elif (user.datadesc[i] == "Batasan_Tinggi"):
-            user.data[i] = str(input("Masukkan " + user.datadesc[i] + ": "))
+    
+    for i in range (aux.length(array_wahana_baru)):
+        if (array_wahana_baru[i] == "ID_Wahana") :
+            user.data[i] = str(input("Masukkan " + array_wahana_baru[i] + ": "))
+        elif (array_wahana_baru[i] == "Nama_Wahana") :
+            user.data[i] = str(input("Masukkan " + array_wahana_baru[i] + ": "))
+        elif (array_wahana_baru[i] == "Harga_Tiket"):
+            user.data[i] = str(input("Masukkan " + array_wahana_baru[i] + ": "))
+        elif (array_wahana_baru[i] == "Batasan_Umur"):
+            user.data[i] = str(input("Masukkan " + array_wahana_baru[i] + ": "))
+        elif (array_wahana_baru[i] == "Batasan_Tinggi"):
+            user.data[i] = str(input("Masukkan " + array_wahana_baru[i] + ": "))
 
-    userfile.dat = user.data
+    wahana_baru_added = aux.konsDot(wahana.data,array_wahana_baru)
+    wahana.data = wahana_baru_added
     print ("")
     print ("Info wahana telah ditambahkan!")
     print ("")
-    return userfile
+    return 
