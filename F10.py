ec5717c0130404ccf958397a46e3ec25f783ebb1
baskorooo <63135932+baskorooo@users.noreply.github.com>
2020-04-25 14:35:22 +0700

    F10
    pls dong udah

--- F10.py
+++ F10.py
@@ -11,32 +11,16 @@ import auxilliary as aux
 #REALISASI FUNGSI/PROSEDUR
 def main(pengguna) :
     # FUNCTION main (input/output userfile : array of array [0..3] of string)
-    # I.S. userfile sudah terdefinisi
-    # F.S. ditambahkan suatu data baru ke userfile
+    # I.S. kritiksaran sudah terdefinisi
+    # F.S. ditambahkan suatu data baru ke kritiksaran
     # KAMUS LOKAL
-    # isDataValid, isUserValid, isValidID : boolean
-    # i : integer
-    # formatcheck : string
-    # ALGORITMA
+    
     kritiksaran = load.use("kritiksaran.csv")
-    isDateValid = False
-    isUserValid = False
-    isValidID = False
-    kritiksaran = load.use("kritiksaran.csv")
-    array_kritik_baru = ["Username","Tanggal Kritik","ID_Wahana","Isi Kritik"]
-    while (isValidID == False) or (isDateValid == False) or (isUserValid == False) :
-        for i in range(aux.length(array_kritik_baru)) :
-            if array_kritik_baru[i] == "Username" :
-                array_kritik_baru[i] = pengguna[1][aux.find_idx(pengguna,"Username")]
-                isUserValid = True
-            elif array_kritik_baru[i] == "Tanggal Kritik" :
-                array_kritik_baru[i] = str(input("Masukkan Tanggal Kritik : "))
-                isDateValid = True
-            elif array_kritik_baru[i] == "ID Wahana" :
-                array_kritik_baru[i] = str(input("Masukkan ID Wahana : "))
-                isValidID = True
-            else :
-                array_kritik_baru[i] = str(input("Masukkan Isi Kritik : "))
+    array_kritik_baru = ["","","",""]
+    array_kritik_baru[0] = pengguna[1][aux.find_idx(pengguna,"Username")]
+    array_kritik_baru[1] = str(input("Masukkan tanggal hari ini :"))
+    array_kritik_baru[2] = str(input("Masukkan ID Wahana :"))
+    array_kritik_baru[3] = str(input("Masukkan kritik dan saran :"))
     kritik_added = aux.konsDot(kritiksaran.data,array_kritik_baru)
     kritiksaran.data = kritik_added
     print("")
